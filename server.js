7e8276171ed1ca40d4df32ba70b69bec5a08bf5a
wlsql852 <wlsql852@naver.com>
2024-07-17 10:09:19 +0900

    serverjs추가

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+
+
+const app = express();
+const PORT = 3000;
+
+
+//pom.xml mvn install -> java
+//npm install
+//py install
+//패키지 관리자
+
+//multer
+//커스텀
+const uploadDir = path.join(__dirname, 'image');
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+}
+
+//
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, uploadDir);
+    },
+
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
+    },
+    //파일을 올릴 때 : 오ㄹ날짜 + 파ㄹ이름
+});
+
+const upload = multer({ storage: storage });
+
+
+app.post('/upload', upload.single('image'), (req, res) => {
+    res.json({ message: 'Image uploaded successfully!', file: req.file });
+});
+
+
+
+app.use(express.static(path.join(__dirname, 'html')));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
